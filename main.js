17a5938cc07696a3b9bb1a3dee1d5a46dd633ac3
Mrlunsuper <irisslee95@gmail.com>
2021-07-09 09:14:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+    xv.disclaimer.display('XNXX.COM',false);
+    var disclaimer_enter = document.getElementById('disclaimer-enter');
+    disclaimer_enter.click();
+});
